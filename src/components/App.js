288fdbf3d63b2196e16c81ecb38c42da22c2c07e
Alicewillbe <zhaolonglong4008@gmail.com>
2020-06-11 13:38:14 -0700

    project uploaded

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../index.css';
+import TopNavBar from './TopNavBar'
+import Main from './Main'
+
+function App() {
+
+  return (
+    <div className="App">
+      <TopNavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
+import nba from 'nba';
+import { PROFILE_PIC_URL_PREFIX } from '../Constants';
+
+class SearchBar extends Component {
+    state = {
+        dataSource: [],
+    };
+
+    handleSearch = (value) => {
+        console.log(value);
+
+        this.setState({
+            dataSource: !value ?
+                [] : nba.searchPlayers(value).map(player => ({
+                    fullName: player.fullName,
+                    playerId: player.playerId,
+                }))
+        });
+    }
+
+    onSelect = (playerName) => {
+        this.props.handleSelectPlayer(playerName);
+    }
+
+    render() {
+        const { dataSource } = this.state;
+        const options = dataSource.map((player) => (
+            <AutoComplete.Option key={player.fullName} value={player.fullName}          className="player-option">
+                <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
+                <span className="player-option-label">{player.fullName}</span>
+            </AutoComplete.Option>
+        ));
+
+        console.log(this.state.dataSource);
+        return (
+            <AutoComplete
+                className="search-bar"
+                size="large"
+                dataSource={options}
+                onSelect={this.onSelect}
+                onSearch={this.handleSearch}
+                placeholder="Search NBA Player"
+                optionLabelProp="value"
+            >
+                <Input suffix={<Icon type="search" className="certain-category-icon" />} />
+            </AutoComplete>
+        );
+    }
+}
+
+export default SearchBar;
